5e124a5c26df227e9f0bf917e31157b1de319f50
Gerald <gm@gerald.host>
2022-10-21 22:48:10 +0100

    feat: contact list

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,14 +1,31 @@
-import { Card } from "@unioncredit/ui";
+import {
+  Card,
+  Table,
+  Pagination,
+  EmptyState,
+  TableRow,
+  TableCell,
+  TableHead,
+  Box,
+  Label,
+} from "@unioncredit/ui";
+import Avatar from "components/shared/Avatar";
+import PrimaryLabel from "components/shared/PrimaryLabel";
+import StatusBadge from "components/shared/StatusBadge";
 import { ContactsType } from "constants";
 
 import { useVouchees } from "providers/VoucheesData";
 import { useVouchers } from "providers/VouchersData";
+import format from "utils/format";
+import { truncateAddress } from "utils/truncateAddress";
 
 export default function ContactList({ type = ContactsType.VOUCHEES }) {
   const { data: vouchees } = useVouchees();
   const { data: vouchers } = useVouchers();
 
-  const contacts = (type === ContactsType.VOUCHEES ? vouchees : vouchers) || [];
+  const contacts = (type === ContactsType.VOUCHEES ? vouchees : vouchers) || {};
+
+  const contactsCount = Object.keys(contacts);
 
   return (
     <Card>
@@ -16,7 +33,42 @@ export default function ContactList({ type = ContactsType.VOUCHEES }) {
         title={`Accounts you trust · ${Object.keys(contacts).length}`}
         subTitle="Addresses you’re currently vouching for"
       />
-      <Card.Body></Card.Body>
+      {contactsCount <= 0 ? (
+        <Card.Body>
+          <EmptyState label="No contacts" />
+        </Card.Body>
+      ) : (
+        <Table>
+          <TableRow>
+            <TableHead></TableHead>
+            <TableHead>Account</TableHead>
+            <TableHead align="center">Status</TableHead>
+            <TableHead align="right">Balance owed (DAI)</TableHead>
+          </TableRow>
+          {Object.keys(contacts).map((address) => (
+            <TableRow key={address}>
+              <TableCell fixedSize>
+                <Avatar size={24} address={address} />
+              </TableCell>
+              <TableCell>
+                <Box direction="vertical">
+                  <Label grey={700} m={0}>
+                    <PrimaryLabel address={address} />
+                  </Label>
+                  <Label size="small" grey={400} m={0}>
+                    {truncateAddress(address)}
+                  </Label>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <StatusBadge address={address} />
+              </TableCell>
+              <TableCell align="right">{format(0)}</TableCell>
+            </TableRow>
+          ))}
+        </Table>
+      )}
+      <Pagination pages={10} activePage={5} onClick={() => alert()} />
     </Card>
   );
 }
