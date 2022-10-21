f8b5412a3f26b00e1d7b37139b6dbb1ff160cbbb
Gerald <gm@gerald.host>
2022-10-21 22:05:35 +0100

    feat: borrower card

--- src/components/stake/BorrowersCard.jsx
+++ src/components/stake/BorrowersCard.jsx
@@ -4,12 +4,23 @@ import {
   Pagination,
   EmptyState,
   TableRow,
+  TableCell,
   TableHead,
   Box,
+  Label,
 } from "@unioncredit/ui";
 
+import format from "utils/format";
+import Avatar from "components/shared/Avatar";
+import { useVouchees } from "providers/VoucheesData";
+import { truncateAddress } from "utils/truncateAddress";
+import PrimaryLabel from "components/shared/PrimaryLabel";
+import StatusBadge from "components/shared/StatusBadge";
+
 export default function BorrowersCard() {
-  const borrowers = [];
+  const { data: vouchee = {} } = useVouchees();
+
+  const borrowers = Object.keys(vouchee).map((address) => ({ address }));
 
   return (
     <Card mt="24px">
@@ -17,7 +28,7 @@ export default function BorrowersCard() {
         title={`Active borrowers · ${borrowers.length}`}
         subTitle="Contacts actively borrowing against your stake"
       />
-      {true ? (
+      {borrowers.length <= 0 ? (
         <Card.Body>
           <EmptyState label="No borrowers" />
         </Card.Body>
@@ -29,7 +40,27 @@ export default function BorrowersCard() {
             <TableHead align="center">Status</TableHead>
             <TableHead align="right">Balance owed (DAI)</TableHead>
           </TableRow>
-          {/* TODO: display borrowers */}
+          {borrowers.map(({ address }) => (
+            <TableRow key={address}>
+              <TableCell fixedSize>
+                <Avatar size={24} address={address} />
+              </TableCell>
+              <TableCell>
+                <Box direction="vertical">
+                  <Label grey={700} m={0}>
+                    <PrimaryLabel address={address} />
+                  </Label>
+                  <Label size="small" grey={400} m={0}>
+                    {truncateAddress(address)}
+                  </Label>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <StatusBadge address={address} />
+              </TableCell>
+              <TableCell align="right">{format(0)}</TableCell>
+            </TableRow>
+          ))}
         </Table>
       )}
       <Pagination pages={10} activePage={5} onClick={() => alert()} />
