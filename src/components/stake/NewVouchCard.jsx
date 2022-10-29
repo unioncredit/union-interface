8b2582dae7e5cd06a76631a51bdd38a35bbe4a08
Gerald <gm@gerald.host>
2022-10-29 15:53:22 +0100

    feat: new vouch card

--- src/components/stake/NewVouchCard.jsx
+++ src/components/stake/NewVouchCard.jsx
@@ -1,6 +1,10 @@
 import { Card, Heading, Button, Label } from "@unioncredit/ui";
+import { VOUCH_MODAL } from "components/modals/VouchModal";
+import { useModals } from "providers/ModalManager";
 
 export default function NewVouchCard() {
+  const { open } = useModals();
+
   return (
     <Card variant="blue" mt="24px">
       <Card.Body>
@@ -9,7 +13,11 @@ export default function NewVouchCard() {
           Vouch for a friend or trusted contact by using your staked assets.
           Make sure it’s someone you really trust.
         </Label>
-        <Button label="Vouch for someone" fluid onClick={() => alert()} />
+        <Button
+          fluid
+          label="Vouch for someone"
+          onClick={() => open(VOUCH_MODAL)}
+        />
       </Card.Body>
     </Card>
   );
