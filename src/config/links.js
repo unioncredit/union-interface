56375463f47f68db5435aabfa2c67e8f2c221447
Gerald <gm@gerald.host>
2022-11-01 14:59:48 +0000

    fix: discord link

--- src/config/links.js
+++ src/config/links.js
@@ -4,7 +4,7 @@ export default {
   website: "https://union.finance/",
   governance: "https://union.finance/governance",
   docs: "https://docs.union.finance/",
-  discord: "https://discord.gg/DWF29rjRJH",
+  discord: "https://discord.gg/fZSmfUshQD",
   github: "https://github.com/unioncredit",
   data: "https://data.union.finance",
 };
