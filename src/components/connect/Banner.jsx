a1c272809ea5edbc805c1fabd6a4eb363997490d
Gerald <gm@gerald.host>
2022-11-16 11:03:40 +0000

    feat: banner margin

--- src/components/connect/Banner.jsx
+++ src/components/connect/Banner.jsx
@@ -4,7 +4,7 @@ import "./Banner.scss";
 export default function Banner() {
   return (
     <div className="ConnectBanner">
-      <Box maxw="411px" px="8px">
+      <Box maxw="411px" w="90%" px="8px">
         <Heading m={0} align="center">
           Welcome to Union, the place to create, manage and grow your on-chain
           credit
