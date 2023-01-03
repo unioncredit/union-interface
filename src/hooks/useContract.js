4baec50480b535d173ef2901fd7632ad76518403
Gerald <gm@gerald.host>
2023-01-03 14:12:48 +0000

    feat: version provider

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -11,6 +11,15 @@ import {
 } from "config/contracts/goerli";
 
 import {
+  userManagerContract as GOERLI_V2_userManagerContract,
+  uTokenContract as GOERLI_V2_uTokenContract,
+  unionContract as GOERLI_V2_unionContract,
+  daiContract as GOERLI_V2_daiContract,
+  comptrollerContract as GOERLI_V2_comptrollerContract,
+  assetManagerContract as GOERLI_V2_assetManagerContract,
+} from "config/contracts/goerli-v2";
+
+import {
   userManagerContract as MAINNET_userManagerContract,
   uTokenContract as MAINNET_uTokenContract,
   unionContract as MAINNET_unionContract,
@@ -29,36 +38,55 @@ import {
   assetManagerContract as ARBITRUM_assetManagerContract,
 } from "config/contracts/arbitrum";
 
+import { useVersion, Versions } from "providers/Version";
+
 export default function useContract(name, chainId) {
   const { chain: connectedChain } = useNetwork();
+  const { version } = useVersion();
 
-  return (
-    {
-      [goerli.id]: {
-        userManager: GOERLI_userManagerContract,
-        uToken: GOERLI_uTokenContract,
-        union: GOERLI_unionContract,
-        dai: GOERLI_daiContract,
-        comptroller: GOERLI_comptrollerContract,
-        assetManager: GOERLI_assetManagerContract,
-      },
-      [mainnet.id]: {
-        governor: governorContract,
-        userManager: MAINNET_userManagerContract,
-        uToken: MAINNET_uTokenContract,
-        union: MAINNET_unionContract,
-        dai: MAINNET_daiContract,
-        comptroller: MAINNET_comptrollerContract,
-        assetManager: MAINNET_assetManagerContract,
-      },
-      [arbitrum.id]: {
-        userManager: ARBITRUM_userManagerContract,
-        uToken: ARBITRUM_uTokenContract,
-        union: ARBITRUM_unionContract,
-        dai: ARBITRUM_daiContract,
-        comptroller: ARBITRUM_comptrollerContract,
-        assetManager: ARBITRUM_assetManagerContract,
-      },
-    }[chainId || connectedChain?.id]?.[name] || {}
-  );
+  const v1Contracts = {
+    [chain.goerli.id]: {
+      userManager: GOERLI_userManagerContract,
+      uToken: GOERLI_uTokenContract,
+      union: GOERLI_unionContract,
+      dai: GOERLI_daiContract,
+      comptroller: GOERLI_comptrollerContract,
+      assetManager: GOERLI_assetManagerContract,
+    },
+    [chain.mainnet.id]: {
+      governor: governorContract,
+      userManager: MAINNET_userManagerContract,
+      uToken: MAINNET_uTokenContract,
+      union: MAINNET_unionContract,
+      dai: MAINNET_daiContract,
+      comptroller: MAINNET_comptrollerContract,
+      assetManager: MAINNET_assetManagerContract,
+    },
+    [chain.arbitrum.id]: {
+      userManager: ARBITRUM_userManagerContract,
+      uToken: ARBITRUM_uTokenContract,
+      union: ARBITRUM_unionContract,
+      dai: ARBITRUM_daiContract,
+      comptroller: ARBITRUM_comptrollerContract,
+      assetManager: ARBITRUM_assetManagerContract,
+    },
+  };
+
+  const v2Contracts = {
+    [chain.goerli.id]: {
+      userManager: GOERLI_V2_userManagerContract,
+      uToken: GOERLI_V2_uTokenContract,
+      union: GOERLI_V2_unionContract,
+      dai: GOERLI_V2_daiContract,
+      comptroller: GOERLI_V2_comptrollerContract,
+      assetManager: GOERLI_V2_assetManagerContract,
+    },
+  };
+
+  const contracts = {
+    [Versions.V1]: v1Contracts,
+    [Versions.V2]: v2Contracts,
+  };
+
+  return contracts[version][chainId || connectedChain?.id]?.[name];
 }
