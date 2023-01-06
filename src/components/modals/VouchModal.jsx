cf0b5d170dc99642506f608c0ddd51d5f41d5fbd
Gerald <gm@gerald.host>
2023-01-06 09:59:54 +0000

    feat: refresh

--- src/components/modals/VouchModal.jsx
+++ src/components/modals/VouchModal.jsx
@@ -12,6 +12,8 @@ import { useMember } from "providers/MemberData";
 import useWrite from "hooks/useWrite";
 import useForm from "hooks/useForm";
 import useLabels from "hooks/useLabels";
+import { useVouchers } from "providers/VouchersData";
+import { useVouchees } from "providers/VoucheesData";
 
 export const VOUCH_MODAL = "vouch-modal";
 
@@ -37,7 +39,11 @@ export default function VouchModal({
   address: initialAddress = null,
 }) {
   const { close } = useModals();
+
   const { refetch: refetchMember } = useMember();
+  const { refetch: refetchVouchers } = useVouchers();
+  const { refetch: refetchVouchees } = useVouchees();
+
   const { values, errors = {}, register } = useForm();
   const { setLabel } = useLabels();
 
@@ -50,6 +56,8 @@ export default function VouchModal({
     enabled: values?.trust?.raw.gt(0) && address,
     onComplete: async () => {
       await refetchMember();
+      await refetchVouchees();
+      await refetchVouchers();
       if (values.name) {
         setLabel(address, values.name);
       }
