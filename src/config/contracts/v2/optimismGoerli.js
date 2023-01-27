0558cd2c6d256cf2228f00e040dcf133859f5d40
Gerald <gm@gerald.host>
2023-01-27 10:37:53 +0000

    feat: update contract addresses

--- src/config/contracts/v2/optimismGoerli.js
+++ src/config/contracts/v2/optimismGoerli.js
@@ -7,36 +7,36 @@ import userManagerAbi from "abis/v2/userManager.json";
 import unionLensAbi from "abis/v2/unionLens.json";
 
 export const userManagerContract = {
-  addressOrName: "0xcF58844990c95070b08afaE75a9CFf7329C562a8",
+  addressOrName: "0x2735f7f27ab27242203665D014FEBD3748CEEDEd",
   contractInterface: userManagerAbi,
 };
 
 export const uTokenContract = {
-  addressOrName: "0x17581B1746ADC0aa6296fE93f70173D5328d1826",
+  addressOrName: "0x146C98A62aAaf96A7051c5828b4c12D9c7B7DDa1",
   contractInterface: uTokenAbi,
 };
 
 export const unionContract = {
-  addressOrName: "0x23B0483E07196c425d771240E81A9c2f1E113D3A",
+  addressOrName: "0x04622FDe6a7C0B19Ff7748eC8882870367530074",
   contractInterface: unionAbi,
 };
 
 export const daiContract = {
-  addressOrName: "0x58069C02723B20412CBF166AbDAeadc6839601C7",
+  addressOrName: "0xD9BB4C7B03C403ece2FeC5B9d188359CE310393D",
   contractInterface: erc20Abi,
 };
 
 export const comptrollerContract = {
-  addressOrName: "0x6Db16f22F1396fd433C236368c0918FA7cdC8299",
+  addressOrName: "0x628F018Dc633557a4B2e27325041a58CD49c47A8",
   contractInterface: comptrollerAbi,
 };
 
 export const assetManagerContract = {
-  addressOrName: "0xa6E0AfC434008dF17E226318fb49B859216b6cbd",
+  addressOrName: "0x79f3AD63E9016eD7b0FB7153509C4CaCba4812D9",
   contractInterface: assetManagerAbi,
 };
 
 export const unionLensContract = {
-  addressOrName: "0x80B504Dc3308385cc75A34AA34fd1A2D8AD93dec",
+  addressOrName: "0x73DEE12FC768Bba7d94906b7e66D31cC206AaaE1",
   contractInterface: unionLensAbi,
 };
