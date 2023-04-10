0226d3138f7cbba5ef48b78e7330b1cf6edd657c
twygod <twygod@gmail.com>
2023-04-10 12:19:38 +0800

    chg: update address on goerli-optimism.

--- src/config/contracts/v2/optimismGoerli.js
+++ src/config/contracts/v2/optimismGoerli.js
@@ -8,17 +8,17 @@ import unionLensAbi from "abis/v2/unionLens.json";
 import vouchFaucetAbi from "abis/vouchFaucet.json";
 
 export const userManagerContract = {
-  address: "0x55a095033FFECcF4E8f712ebda0850c7f3dF2E25",
+  address: "0xC3DAd06E1459aB76a29Cc52aA831B0cE68d53B8b",
   abi: userManagerAbi,
 };
 
 export const uTokenContract = {
-  address: "0x10f35722588571BB109820Be6F515634336512Ec",
+  address: "0xd559c581cbA5Eb2bA463c17C7E6243894eb17719",
   abi: uTokenAbi,
 };
 
 export const unionContract = {
-  address: "0x457E066FC7D6c7567f21Fc96E7a713d02cE58D61",
+  address: "0xfa6C3cF088Cc770a05773a5d294aa75b9fdFCEf5",
   abi: unionAbi,
 };
 
@@ -28,21 +28,21 @@ export const daiContract = {
 };
 
 export const comptrollerContract = {
-  address: "0x1c970FAC1D3BDcB9Bc984A2f39Df769760AeE0ac",
+  address: "0xFBE7b0A8D6788B6293376d6B4d31A4c836fED34B",
   abi: comptrollerAbi,
 };
 
 export const assetManagerContract = {
-  address: "0x099a2B3f5A62053D6Aa032A9AeC410f7d51713e3",
+  address: "0x380f21267C0c0D62619d0b211191654F7201eD47",
   abi: assetManagerAbi,
 };
 
 export const unionLensContract = {
-  address: "0x38FE97157C531a677b7cF86b75ddE40d6B1bb7F0",
+  address: "0x5386c7858a70E75E9f86b330F3DA8FFd64ffA6D1",
   abi: unionLensAbi,
 };
 
 export const vouchFaucetContract = {
-  address: "0xA36A707ebb8a41c50875cE76d44C1E21A1fd7556",
+  address: "0x411149bC2d8B302EE9d1D55AC2Df1c1EC9e6D3fA",
   abi: vouchFaucetAbi,
 };
