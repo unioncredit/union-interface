22c2ec9768e1a95a947c9dfecb97c663218011a3
barbs <115395121+0xbarbs@users.noreply.github.com>
2023-04-24 14:42:24 +0100

    UNI-1326 Updated opgoerli block speed constants #95

--- src/constants.js
+++ src/constants.js
@@ -55,7 +55,7 @@ export const BlocksPerYear = {
   [mainnet.id]: 2407328,
   [arbitrum.id]: 2407328,
   [goerli.id]: 2407328,
-  [optimismGoerli.id]: 31536000, //second per year
+  [optimismGoerli.id]: 15770000,
 };
 
 export const EIP3770 = {
