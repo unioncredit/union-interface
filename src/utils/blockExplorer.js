f90c5fca60f561b56f4e14edfe1a78d4a82117e0
Gerald <gm@gerald.host>
2023-04-26 09:55:18 +0100

    feat: block explorer

--- src/utils/blockExplorer.js
+++ src/utils/blockExplorer.js
@@ -1,10 +1,17 @@
-import { mainnet, goerli, arbitrum, optimismGoerli } from "wagmi/chains";
+import {
+  mainnet,
+  goerli,
+  arbitrum,
+  optimismGoerli,
+  optimism,
+} from "wagmi/chains";
 
 const urls = {
   [mainnet.id]: "https://etherscan.io",
   [goerli.id]: "https://goerli.etherscan.io",
   [arbitrum.id]: "https://arbiscan.io",
   [optimismGoerli.id]: "https://goerli-optimism.etherscan.io",
+  [optimism.id]: "https://optimism.etherscan.io",
 };
 
 export function blockExplorerTx(chainId, hash) {
