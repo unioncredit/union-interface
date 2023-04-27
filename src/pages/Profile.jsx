e3a7cd778342514ed3d677ab10dfba4e1e415fd8
Gerald <gm@gerald.host>
2023-04-27 06:49:11 +0100

    fix: profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -227,7 +227,9 @@ export default function Profile() {
   // Profile pages support EIP3770 addresses so we need to check if
   // it starts with eth: or goe: or arb1: then parse out the address
   const addressOrEnsParts = addressOrEnsParam.split(":");
-  const [tag, addressOrEns] = addressOrEnsParam.match(/^(eth|goe|arb1|optgoe):/)
+  const [tag, addressOrEns] = addressOrEnsParam.match(
+    /^(eth|goe|arb1|optgoe|opt):/
+  )
     ? addressOrEnsParts
     : [EIP3770[mainnet.id], addressOrEnsParam];
 
