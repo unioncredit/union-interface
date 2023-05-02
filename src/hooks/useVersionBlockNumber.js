f5ea3b8ea213b057b8c752d88e7361f03c1ab4fb
0xbarbs <0xbarbs@proton.me>
2023-05-02 21:15:14 +0100

    fix: factor in repay block number diff for versioned block number

--- src/hooks/useVersionBlockNumber.js
+++ src/hooks/useVersionBlockNumber.js
@@ -1,10 +1,11 @@
 import { useBlockNumber } from "wagmi";
 import { useVersion } from "providers/Version";
+import { arbitrum, mainnet } from "wagmi/chains";
 
 export function useVersionBlockNumber({ chainId }) {
   const { isV2 } = useVersion();
   const { data: blockNumber } = useBlockNumber({
-    chainId,
+    chainId: chainId === arbitrum.id ? mainnet.id : chainId,
   });
 
   const unixTimestamp = Math.round(Date.now() / 1000);
