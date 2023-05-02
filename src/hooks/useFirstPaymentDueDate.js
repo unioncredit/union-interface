542899ab29c7e4624b58da94bbfc2411f66119a0
0xbarbs <0xbarbs@proton.me>
2023-05-02 21:10:53 +0100

    remove debug log

--- src/hooks/useFirstPaymentDueDate.js
+++ src/hooks/useFirstPaymentDueDate.js
@@ -10,8 +10,6 @@ export default function useFirstPaymentDueDate() {
   const { data: protocol } = useProtocol();
   const { overdueTime = ZERO, overdueBlocks = ZERO } = protocol;
 
-  console.log(protocol);
-
   const milliseconds = (isV2 ? overdueTime : overdueBlocks).mul(
     BlockSpeed[chain.id]
   );
