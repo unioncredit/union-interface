440001644408c8db97978a263f2fb22b730a961c
Jacob Shiach <kingjacob@gmail.com>
2023-05-13 15:28:07 -0500

    Update StakeModal.jsx

--- src/components/modals/StakeModal.jsx
+++ src/components/modals/StakeModal.jsx
@@ -170,7 +170,7 @@ export default function StakeModal({ type: initialType = StakeType.STAKE }) {
               args: [amount.raw],
               enabled: !isErrored,
               contract: "userManager",
-              overrides: { gasLimit: 1_000_001 },
+              overrides: { gasLimit: 5_000_001 },
               method: type === StakeType.STAKE ? "stake" : "unstake",
               label: `${type === StakeType.STAKE ? "Stake" : "Unstake"} ${
                 amount.display
