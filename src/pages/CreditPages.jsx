01005b9cfcf2a8d27edd2da1a81dfbb5da46bd2d
0xbarbs <0xbarbs@proton.me>
2023-05-18 19:50:35 +0100

    fix: set initialActive for credit pages seg control

--- src/pages/CreditPages.jsx
+++ src/pages/CreditPages.jsx
@@ -10,31 +10,40 @@ export const PAGES = {
   BORROW: {
     id: "borrow",
     maxw: "653px",
+    initialActive: 0,
     component: <BorrowPage />,
   },
   STAKE: {
     id: "stake",
     maxw: "653px",
+    initialActive: 1,
     component: <StakePage />,
   },
   CONTACTS_PROVIDING: {
     id: "contacts",
     maxw: "none",
+    initialActive: 2,
     component: <ContactsPage type={ContactsType.VOUCHEES} />,
   },
   CONTACTS_RECEIVING: {
     id: "contacts",
     maxw: "none",
+    initialActive: 2,
     component: <ContactsPage type={ContactsType.VOUCHERS} />,
   },
 };
 
 export default function CreditPages({ page }) {
-  const { id, maxw, component } = page;
+  const { id, maxw, initialActive, component } = page;
 
   return (
     <Layout.Columned align="center" maxw={maxw}>
-      <CreditSegmentedControl maxw="653px" value={id} />
+      <CreditSegmentedControl
+        value={id}
+        maxw="653px"
+        initialActive={initialActive}
+      />
+
       {component}
     </Layout.Columned>
   );
