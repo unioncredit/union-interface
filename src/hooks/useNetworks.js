a114152f3975ab07447b296db14b5ef3c483a980
0xbarbs <0xbarbs@proton.me>
2023-06-07 11:39:56 +0100

    fix: show get started page and network select options

--- src/hooks/useNetworks.js
+++ src/hooks/useNetworks.js
@@ -2,10 +2,10 @@ import { useVersion, Versions } from "providers/Version";
 import { networks as allNetworks } from "config/networks";
 import { useMemo } from "react";
 
-export default function useNetworks(all = false) {
+export default function useNetworks(all = false, forceVersion = null) {
   const { version } = useVersion();
   if (all) {
     return useMemo(() => [...allNetworks[Versions.V1], ...allNetworks[Versions.V2]], []);
   }
-  return useMemo(() => allNetworks[version], [version]);
+  return useMemo(() => allNetworks[forceVersion ?? version], [forceVersion, version]);
 }
