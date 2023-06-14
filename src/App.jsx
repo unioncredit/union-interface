216456a24479fac8be319591c9ab4d9ce3ba1a6a
0xbarbs <0xbarbs@proton.me>
2023-06-14 11:46:03 +0100

    fix: improved 404 route matching

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,17 @@ export default function App() {
 
   const isGeneralRoute = Boolean(matchRoutes(generalRoutes, location));
 
-  const is404 = !allRoutes.some((r) => r.path === location.pathname);
+  const is404 = !allRoutes.some((r) => {
+    // Exact match
+    if (r.path === location.pathname) return true;
+
+    // Check for paths parameters
+    if (r.path.includes(":")) {
+      const index = r.path.indexOf(":");
+      const substring = index !== -1 ? r.path.substring(0, index) : r.path;
+      return location.pathname.startsWith(substring);
+    }
+  });
 
   if (!version || ((chain?.unsupported || !isConnected) && !isGeneralRoute)) {
     return (
