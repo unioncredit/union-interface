8b8719d2150e59a37cd5fae3eafa00977ee77840
0xbarbs <0xbarbs@proton.me>
2023-07-28 21:01:31 +0100

    feat: integrated stepped section component and created new vouch modal

--- src/config/contracts/v2/optimism.js
+++ src/config/contracts/v2/optimism.js
@@ -6,6 +6,7 @@ import uTokenAbi from "abis/v2/uToken.json";
 import userManagerAbi from "abis/v2/userManager.json";
 import unionLensAbi from "abis/v2/unionLens.json";
 import vouchFaucetAbi from "abis/vouchFaucet.json";
+import vouchNoteAbi from "abis/v2/vouchNote.json";
 
 export const userManagerContract = {
   address: "0x8E195D65b9932185Fcc76dB5144534e0f3597628",
@@ -45,4 +46,9 @@ export const unionLensContract = {
 export const vouchFaucetContract = {
   address: "",
   abi: vouchFaucetAbi,
+};
+
+export const vouchNoteContract = {
+  address: "0xE81911b311AdED2dAb287C61D5FebA3b2e2dE6c2",
+  abi: vouchNoteAbi,
 };
