e0a40b5833c8cc4b15042967693d808da76110a7
0xbarbs <0xbarbs@proton.me>
2023-09-16 16:39:24 +0100

    feat: added social/account type verification to profile pages

---
+++ src/components/profile/ProfileHeader.jsx
@@ -0,0 +1,160 @@
+import "./ProfileHeader.scss";
+
+import {
+  Text,
+  Badge,
+  Box,
+  Heading,
+  Avatar as UiAvatar,
+  BadgeRow,
+  LinkOutIcon,
+  BadgeIndicator,
+  ProfileIcon,
+} from "@unioncredit/ui";
+import { BigNumber } from "ethers";
+
+import { Avatar, PrimaryLabel } from "../shared";
+import { blockExplorerAddress } from "utils/blockExplorer";
+import useCopyToClipboard from "hooks/useCopyToClipboard";
+import { useVersionBlockNumber } from "hooks/useVersionBlockNumber";
+import { EIP3770, ZERO } from "constants";
+import { useProtocol } from "providers/ProtocolData";
+import { useVersion } from "providers/Version";
+import useNetworks from "../../hooks/useNetworks";
+import { useEffect, useState } from "react";
+import { ReactComponent as FarcasterIcon } from "../../images/verification/farcaster.svg";
+import { ReactComponent as TwitterIcon } from "../../images/verification/twitter.svg";
+import { ReactComponent as LensIcon } from "../../images/verification/lens.svg";
+import useResponsive from "../../hooks/useResponsive";
+import { truncateAddress } from "../../utils/truncateAddress";
+
+const ProfileAddress = ({ profileMember, chainId }) => {
+  const { isV2 } = useVersion();
+  const { data: protocol } = useProtocol();
+  const { data: blockNumber } = useVersionBlockNumber({
+    chainId,
+  });
+
+  const { isMember = false, isOverdue = false, lastRepay = ZERO } = profileMember;
+  const { overdueTime = ZERO, overdueBlocks = ZERO, maxOverdueTime = ZERO } = protocol;
+
+  const maxOverdueTotal = (overdueTime || overdueBlocks).add(maxOverdueTime);
+
+  const isMaxOverdue =
+    isOverdue &&
+    lastRepay &&
+    isV2 &&
+    BigNumber.from(blockNumber).gte(lastRepay.add(maxOverdueTotal));
+
+  return isMaxOverdue ? (
+    <BadgeIndicator label="Write-Off" color="red500" textColor="red500" />
+  ) : isMember ? (
+    <BadgeIndicator label="Member" color="blue500" />
+  ) : (
+    <BadgeIndicator label="Non-member" />
+  );
+};
+
+export default function ProfileHeader({ address, profileMember, chainId }) {
+  const networks = useNetworks(true);
+  const { isMobile, isTablet } = useResponsive();
+
+  const [copiedAddress, copyAddress] = useCopyToClipboard();
+  const [data, setData] = useState(null);
+
+  const targetNetwork = networks.find((network) => network.chainId === Number(chainId));
+
+  useEffect(() => {
+    fetch(`http://api.union.finance:8080/address/${address}`)
+      .then((res) => res.json())
+      .then(setData)
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, [address]);
+
+  return (
+    <Box
+      align="flex-start"
+      direction={isMobile ? "vertical" : "horizontal"}
+      className="ProfileHeader"
+      style={{
+        background: `radial-gradient(circle at 60% -20%, rgba(207,207,254,1) 40%, rgba(251,214,93,1) 60%, rgba(124,239,117,1) 100%)`,
+      }}
+    >
+      <Box className="ProfileHeader__avatar">
+        <Avatar address={address} size={112} />
+
+        <div className="ProfileHeader__avatar__network">
+          <UiAvatar src={targetNetwork?.imageSrc} size={28} />
+        </div>
+      </Box>
+
+      <Box className="ProfileHeader__content" direction="vertical">
+        <Heading mb={0}>
+          <PrimaryLabel address={address} shouldTruncate={false} />
+        </Heading>
+
+        <Box mt="8px" align="center" className="ProfileHeader__address">
+          <BadgeRow>
+            <Badge
+              mr="4px"
+              color="grey"
+              onClick={() => copyAddress(address)}
+              label={
+                copiedAddress
+                  ? "Address copied to clipboard"
+                  : isTablet
+                  ? truncateAddress(address)
+                  : `${EIP3770[chainId]}:${address}`
+              }
+            />
+
+            <ProfileAddress profileMember={profileMember} chainId={chainId} />
+          </BadgeRow>
+
+          <a href={blockExplorerAddress(chainId, address)} target="_blank" rel="noreferrer">
+            <LinkOutIcon width="24px" />
+          </a>
+        </Box>
+
+        {data && (
+          <Box className="ProfileHeader__verification">
+            <VerificationBadge
+              type="account"
+              label={data.account.is_contract_address ? "Contract address" : "EOA"}
+            />
+
+            {data.socials?.map((item, idx) => (
+              <VerificationBadge key={idx} type={item.dappname} label={item.profileName} />
+            ))}
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+}
+
+const VerificationBadge = ({ type, label }) => {
+  const Icon = () => {
+    switch (type) {
+      case "account":
+        return <ProfileIcon />;
+      case "farcaster":
+        return <FarcasterIcon />;
+      case "twitter":
+        return <TwitterIcon />;
+      case "lens":
+        return <LensIcon />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box className="ProfileHeader__verification__item" justify="center">
+      <Icon />
+      <Text>{label}</Text>
+    </Box>
+  );
+};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,96 +1,23 @@
 import "./Profile.scss";
 
-import {
-  Badge,
-  Box,
-  Button,
-  Card,
-  Divider,
-  Grid,
-  Heading,
-  Text,
-  Avatar as UiAvatar,
-  BadgeRow,
-  NumericalBlock,
-  LinkOutIcon,
-  VouchIcon,
-  SwitchIcon,
-  Layout,
-  ManageIcon,
-  CancelIcon,
-  BadgeIndicator,
-} from "@unioncredit/ui";
-import { useAccount, useEnsAddress, useNetwork, useSwitchNetwork } from "wagmi";
+import { Box, Card, Divider, Grid, Heading, Text, NumericalBlock, Layout } from "@unioncredit/ui";
+import { useAccount, useEnsAddress, useNetwork } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { Helmet } from "react-helmet";
 import { isAddress } from "ethers/lib/utils";
-import { Link as RouterLink, useNavigate, useParams } from "react-router-dom";
-
-import { Avatar, ConnectButton, PrimaryLabel } from "components/shared";
-import { truncateAddress } from "utils/truncateAddress";
+import { useParams } from "react-router-dom";
 import { useMemberData } from "providers/MemberData";
-import { EIP3770, ZERO, ZERO_ADDRESS } from "constants";
-import { compareAddresses } from "utils/compare";
-import { VOUCH_MODAL } from "components/modals/VouchModal";
-import { useModals } from "providers/ModalManager";
-import { blockExplorerAddress } from "utils/blockExplorer";
-import useNetworks from "hooks/useNetworks";
-import useCopyToClipboard from "hooks/useCopyToClipboard";
+import { EIP3770, ZERO_ADDRESS } from "constants";
 import ProfileGovernanceStats from "components/profile/ProfileGovernanceStats";
-import { getVersion, useVersion } from "providers/Version";
-import { useProtocol } from "../providers/ProtocolData";
-import { BigNumber } from "ethers";
-import { useVersionBlockNumber } from "../hooks/useVersionBlockNumber";
-import { PUBLIC_WRITE_OFF_DEBT_MODAL } from "../components/modals/PublicWriteOffDebtModal";
+import { getVersion } from "providers/Version";
+import ProfileHeader from "../components/profile/ProfileHeader";
 
-function ProfileInner({ profileMember = {}, connectedMember = {}, chainId }) {
-  const navigate = useNavigate();
-
-  const { open } = useModals();
-  const { isV2 } = useVersion();
-  const { data: protocol } = useProtocol();
+function ProfileInner({ profileMember = {}, chainId }) {
   const { chain: connectedChain } = useNetwork();
-  const { switchNetworkAsync } = useSwitchNetwork();
-  const { data: blockNumber } = useVersionBlockNumber({
-    chainId,
-  });
 
-  const [copied, copy] = useCopyToClipboard();
-  const [copiedAddress, copyAddress] = useCopyToClipboard();
-
-  const networks = useNetworks(true);
-
-  const {
-    isMember = false,
-    isOverdue = false,
-    stakerAddresses = [],
-    borrowerAddresses = [],
-    lastRepay = ZERO,
-  } = profileMember;
-
-  const { overdueTime = ZERO, overdueBlocks = ZERO, maxOverdueTime = ZERO } = protocol;
-
-  const maxOverdueTotal = (overdueTime || overdueBlocks).add(maxOverdueTime);
-  const isMaxOverdue =
-    isOverdue &&
-    lastRepay &&
-    isV2 &&
-    BigNumber.from(blockNumber).gte(lastRepay.add(maxOverdueTotal));
+  const { stakerAddresses = [], borrowerAddresses = [] } = profileMember;
 
   const address = profileMember.address || ZERO_ADDRESS;
-
-  const { borrowerAddresses: connectedMemberBorrowerAddresses = [] } = connectedMember;
-
-  const alreadyVouching = connectedMemberBorrowerAddresses.some((borrower) =>
-    compareAddresses(borrower, address)
-  );
-
-  const isSelf = compareAddresses(
-    profileMember.address || ZERO_ADDRESS,
-    connectedMember.address || ZERO_ADDRESS
-  );
-
-  const targetNetwork = networks.find((network) => network.chainId === Number(chainId));
 
   return (
     <Box fluid justify="center" direction="vertical" className="ProfileInner">
@@ -98,106 +25,8 @@ function ProfileInner({ profileMember = {}, connectedMember = {}, chainId }) {
         Profile Header 
       *--------------------------------------------------------------*/}
       <Card mb="24px">
-        <Card.Body>
-          <Box direction="vertical" align="center">
-            <div className="ProfileInner__avatar">
-              <Avatar address={address} size={56} />
-              <div className="ProfileInner__avatar__network">
-                <UiAvatar src={targetNetwork?.imageSrc} size={24} />
-              </div>
-            </div>
-            <Heading mt="8px" mb={0}>
-              <PrimaryLabel address={address} shouldTruncate={false} />
-            </Heading>
-            <Box mt="8px" align="center">
-              <BadgeRow>
-                <Badge
-                  mr="4px"
-                  color="grey"
-                  onClick={() => copyAddress(address)}
-                  label={copiedAddress ? "Copied!" : truncateAddress(address)}
-                />
-                {isMaxOverdue ? (
-                  <BadgeIndicator label="Write-Off" color="red500" textColor="red500" />
-                ) : isMember ? (
-                  <BadgeIndicator label="Member" color="blue500" />
-                ) : (
-                  <BadgeIndicator label="Non-member" />
-                )}
-              </BadgeRow>
-              <a href={blockExplorerAddress(chainId, address)} target="_blank" rel="noreferrer">
-                <LinkOutIcon width="24px" />
-              </a>
-            </Box>
-            {/*--------------------------------------------------------------
-              Vouch/Connect Button 
-            *--------------------------------------------------------------*/}
-            <ConnectButton
-              buttonProps={{
-                fluid: true,
-                label: "Connect Wallet",
-                style: {
-                  marginTop: "20px",
-                },
-              }}
-              connectedElement={
-                isSelf ? (
-                  <Button fluid to="/" mt="20px" as={RouterLink} label="Go to Dashboard" />
-                ) : connectedChain?.id !== Number(chainId) ? (
-                  <Button
-                    fluid
-                    mt="20px"
-                    color="blue"
-                    icon={SwitchIcon}
-                    label={`Switch to ${targetNetwork?.label}`}
-                    onClick={() => switchNetworkAsync(targetNetwork?.chainId)}
-                  />
-                ) : isMaxOverdue ? (
-                  <Button
-                    fluid
-                    mt="20px"
-                    icon={CancelIcon}
-                    label="Write-off debt"
-                    className="WriteOffButton"
-                    onClick={() =>
-                      open(PUBLIC_WRITE_OFF_DEBT_MODAL, {
-                        address: profileMember.address,
-                      })
-                    }
-                  />
-                ) : alreadyVouching ? (
-                  <Button
-                    fluid
-                    mt="20px"
-                    icon={ManageIcon}
-                    label="Manage Contact"
-                    onClick={() => navigate(`/contacts/providing?address=${address}`)}
-                  />
-                ) : !connectedMember.isMember ? (
-                  <Button fluid to="/" mt="20px" as={RouterLink} label="Register to vouch" />
-                ) : (
-                  <Button
-                    fluid
-                    mt="20px"
-                    icon={VouchIcon}
-                    onClick={() => {
-                      open(VOUCH_MODAL, { address });
-                    }}
-                    label={targetNetwork ? `Vouch on ${targetNetwork.label}` : "Vouch"}
-                  />
-                )
-              }
-            />
-            <Button
-              fluid
-              mt="8px"
-              icon={RouterLink}
-              color="secondary"
-              variant="light"
-              label={copied ? "Copied" : "Copy profile link"}
-              onClick={() => copy(window.location.host + window.location.hash)}
-            />
-          </Box>
+        <Card.Body p={0}>
+          <ProfileHeader address={address} profileMember={profileMember} chainId={chainId} />
         </Card.Body>
       </Card>
       {/*--------------------------------------------------------------
@@ -267,7 +96,7 @@ export default function Profile() {
       <Helmet>
         <title>{`Profile ${address} | Union Credit Protocol`}</title>
       </Helmet>
-      <Layout.Columned mt="24px" maxw="653px">
+      <Layout.Columned mt="24px" maxw="985px">
         <ProfileInner
           chainId={chainId}
           profileMember={profileMember}
