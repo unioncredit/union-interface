a8e216c0afe2df3a57371cae8f2b58956e77f81c
barbs <115395121+0xbarbs@users.noreply.github.com>
2023-10-23 11:42:52 +0100

    UNI-1631 Borrow modal payment due date tweak #243

--- src/components/modals/BorrowModal.jsx
+++ src/components/modals/BorrowModal.jsx
@@ -168,7 +168,9 @@ export default function BorrowModal() {
               },
               {
                 label: "Payment due",
-                value: `${format(minPayment)} DAI · ${firstPaymentDueDate}`,
+                value: amount.raw.lte(0)
+                  ? "N/A"
+                  : `${format(minPayment)} DAI · ${firstPaymentDueDate}`,
                 tooltip: {
                   content:
                     "The amount and date of your next minimum payment in order to not enter a default state",
