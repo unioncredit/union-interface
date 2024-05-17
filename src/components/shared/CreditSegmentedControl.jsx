7cc92382ec1a4db8276dd97cfc15f103ba8d106f
0xbarbs <0xbarbs@proton.me>
2024-05-17 23:06:55 +0100

    feat: reworked contacts table filtering logic to use url query params

--- src/components/shared/CreditSegmentedControl.jsx
+++ src/components/shared/CreditSegmentedControl.jsx
@@ -1,45 +1,49 @@
 import "./CreditSegmentedControl.scss";
 
-import { Link } from "react-router-dom";
-import {
-  AddressBookIcon,
-  IdentityIcon,
-  SegmentedControl,
-  StakeIcon,
-} from "@unioncredit/ui";
+import { Link, useSearchParams } from "react-router-dom";
+import { AddressBookIcon, IdentityIcon, SegmentedControl, StakeIcon } from "@unioncredit/ui";
 import { Links } from "constants";
 
-export const CreditSegmentedControl = ({ value, ...props }) => (
-  <SegmentedControl
-    fluid
-    m="24px auto"
-    size="large"
-    variant="rounded"
-    value={value}
-    className="CreditSegmentedControl"
-    {...props}
-    items={[
-      {
-        id: "borrow",
-        label: "Borrow",
-        to: Links.CREDIT,
-        as: Link,
-        icon: IdentityIcon,
-      },
-      {
-        id: "stake",
-        label: "Stake",
-        to: Links.STAKE,
-        as: Link,
-        icon: StakeIcon,
-      },
-      {
-        id: "contacts",
-        label: "Contacts",
-        to: Links.CONTACTS,
-        as: Link,
-        icon: AddressBookIcon,
-      },
-    ]}
-  />
-);
+export const CreditSegmentedControl = ({ value, ...props }) => {
+  const [searchParams] = useSearchParams();
+
+  const filters = searchParams
+    .getAll("filters")
+    .map((f) => `filters=${f}`)
+    .join("&");
+
+  return (
+    <SegmentedControl
+      fluid
+      m="24px auto"
+      size="large"
+      variant="rounded"
+      value={value}
+      className="CreditSegmentedControl"
+      {...props}
+      items={[
+        {
+          id: "borrow",
+          label: "Borrow",
+          to: Links.CREDIT,
+          as: Link,
+          icon: IdentityIcon,
+        },
+        {
+          id: "stake",
+          label: "Stake",
+          to: Links.STAKE,
+          as: Link,
+          icon: StakeIcon,
+        },
+        {
+          id: "contacts",
+          label: "Contacts",
+          to: filters.length > 0 ? `${Links.CONTACTS}?${filters}` : Links.CONTACTS,
+          as: Link,
+          icon: AddressBookIcon,
+        },
+      ]}
+    />
+  );
+};
