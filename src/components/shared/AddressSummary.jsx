f59a579527eed8bad2b6e365d56bbd58400c0cf9
0xbarbs <0xbarbs@proton.me>
2024-06-14 21:34:09 +0100

    feat: test

--- src/components/shared/AddressSummary.jsx
+++ src/components/shared/AddressSummary.jsx
@@ -93,14 +93,14 @@ export function AddressSummary({ address, chainId: chainIdProp, allowEdit = fals
                 ref={(input) => input && input.focus()}
                 type="text"
                 value={labelText}
-                onfocusout={(e) =>
-                  (!e.relatedTarget || !e.relatedTarget.classList.contains("AliasButton")) &&
-                  setEditMode(false)
-                }
-                onBlur={(e) =>
-                  (!e.relatedTarget || !e.relatedTarget.classList.contains("AliasButton")) &&
-                  setEditMode(false)
-                }
+                // onfocusout={(e) =>
+                //   (!e.relatedTarget || !e.relatedTarget.classList.contains("AliasButton")) &&
+                //   setEditMode(false)
+                // }
+                // onBlur={(e) =>
+                //   (!e.relatedTarget || !e.relatedTarget.classList.contains("AliasButton")) &&
+                //   setEditMode(false)
+                // }
                 onKeyDown={handleKeyDown}
                 onChange={(e) => setLabelText(e.target.value)}
                 className="AddressSummary__alias-input"
