1e3ba93661197b080e5f012f5ca95ff9c8e4ee8e
Jacob Shiach <kingjacob@gmail.com>
2025-04-15 15:57:20 +0200

    Update links.js

--- src/config/links.js
+++ src/config/links.js
@@ -1,10 +1,12 @@
 export default {
-  twitter: "https://twitter.com/unionprotocol",
+  twitter: "https://x.com/unionprotocol",
   blog: "https://union.mirror.xyz",
   website: "https://union.finance/",
   governance: "https://union.finance/governance",
   docs: "https://docs.union.finance/",
   discord: "https://discord.gg/fZSmfUshQD",
   github: "https://github.com/unioncredit",
-  data: "https://data.union.finance/optimism",
+  data: "https://data.union.finance/base",
+  warpcast: "http://warpcast.com/union",
+  
 };
